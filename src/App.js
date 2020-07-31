00d3ad13381afe542841319a876599777e059d74
Elizabeth <42512400+Lezette@users.noreply.github.com>
2020-07-31 12:24:12 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import Heading from './components/Heading';
-import Search from './components/Search';
-import FilterSearch from './components/FilterSearch';
-import Subreddit from './components/Subreddit';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Container from '@material-ui/core/Container';
+// import Box from '@material-ui/core/Box';
+// import Grid from '@material-ui/core/Grid';
+// import Heading from './components/Heading';
+// import Search from './components/Search';
+// import FilterSearch from './components/FilterSearch';
+// import Subreddit from './components/Subreddit';
 
 export default function App() {
 	return (
 		<div>
-			<Heading />
+			{/* <Heading />
 			<CssBaseline />
 			<Box m={1} />
 			<Container maxWidth="sm">
@@ -20,7 +20,8 @@ export default function App() {
 					<FilterSearch />
 				</Grid>
 			</Container>
-			<Subreddit />
+			<Subreddit /> */}
+			Hello World
 		</div>
 	);
 }
